bdfe957587965e6dc3c134f282cacfe8d8be5c60
Praveen Verma <praveen.verma@hcl.com>
2020-03-05 17:32:19 +0530

    extends SpringBootServletInitializer in main class. for access all api on jenkins deployed server

--- src/main/java/com/hcl/ecom/rolebasedoauth2/RoleBasedOauth2Application.java
+++ src/main/java/com/hcl/ecom/rolebasedoauth2/RoleBasedOauth2Application.java
@@ -2,12 +2,12 @@ package com.hcl.ecom.rolebasedoauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class RoleBasedOauth2Application {
+public class RoleBasedOauth2Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RoleBasedOauth2Application.class, args);
